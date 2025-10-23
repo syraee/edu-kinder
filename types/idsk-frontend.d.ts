85ff10edad8cc335d915ec8aacd98dbcfe05db17
martaan913 <martaan913@gmail.com>
2025-10-23 11:51:32 +0200

    Move frontend into frontend/ folder, created backend folder.

--- types/idsk-frontend.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@id-sk/frontend/idsk/all.js' {
-  export const initAll: () => void;
-  const x: any; export default x;
-}
-declare module '@id-sk/frontend/idsk/all' {
-  const x: any; export default x;
-}
