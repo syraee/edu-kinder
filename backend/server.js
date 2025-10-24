4cb2835ec143f4a0c9a778e06f1bfeb34ec23afa
martaan913 <martaan913@gmail.com>
2025-10-24 10:14:41 +0200

    created errorHandler.js for errors in routing, handler returns JSON with fields success, statusCode and message.

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(express.json());
-
-// Routes
+// Requires
 const routes = require("./src/routes/routes");
+const errorHandler = require("./src/middleware/errorHandler");
+
 app.use("/api", routes);
+app.use((req, res, next) => {
+    const err = new Error("Route not found");
+    err.statusCode = 404;
+    next(err);
+});
+app.use(errorHandler);
+app.use(express.json());
 
 // Start server
 app.listen(PORT, () => {
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-// GET /api/users
-router.get("/", (req, res) => {
-    res.send("List of users");
+router.get("/", (req, res, next) => {
+    try {
+        const fail = false;
+        if (fail) {
+            const error = new Error("Failed to fetch users");
+            error.statusCode = 400;
+            return next(error);
+        }
+
+        res.json({
+            success: true,
+            data: ["Alice", "Bob", "Charlie"]
+        });
+    } catch (err) {
+        next(err); // všetky neočakávané chyby posielame do error handlera
+    }
 });
 
 // POST /api/users
