85e10bdcdbd18ddc9640be3178480c9798d1714c
AdamMacko <145450207+AdamMacko@users.noreply.github.com>
2026-01-17 20:28:42 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
+app.options("/*", cors(corsOptions)); 
 
 // Loguj hneď na začiatku (uvidíš aj preflight)
 app.use((req, _res, next) => {
