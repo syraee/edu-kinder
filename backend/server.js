1fd919591024f9d27e848d94827d053c737626e6
Alžbeta Andrejková <135422919+syraee@users.noreply.github.com>
2026-01-22 16:01:39 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization","Cookie"],
 };
 
 app.use(cors(corsOptions));
